87b73dc6b346bdeb8d892f27e25f7f255996b6e6
ayrton <ayrton.mercado@gmail.com>
2025-06-12 16:11:32 -0400

    fixed relative paths for 4 modules

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,10 +2,10 @@
 
 {
   imports = [
-    ./home-manager/modules/vscode.nix
-    ./home-manager/modules/nushell.nix
-    ./home-manager/modules/git.nix
-    ./home-manager/modules/zellij.nix
+    ./modules/vscode.nix
+    ./modules/nushell.nix
+    ./modules/git.nix
+    ./modules/zellij.nix
   ];
 
   programs.home-manager.enable = true;
