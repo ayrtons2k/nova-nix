04af1071315431455c5d11620b598ecf8c4baa2e
Ayrton Mercado <ayrton.mercado@gmail.com>
2025-07-04 07:59:35 -0400

    Changed enabled nvidia power management to try and prevent reboot crashes after hybernation/sleep, to no avail

--- configuration.nix
+++ configuration.nix
@@ -28,7 +28,7 @@ in
 
     nvidia = {
       modesetting.enable = true;
-      powerManagement.enable = false;
+      powerManagement.enable = true;
       powerManagement.finegrained = false;
       open = false;  # Use proprietary drivers
       nvidiaSettings = true;
