bd4bb42d5b6ebfb5487f49ee7b0f8525aaf802ba
Ayrton <ayrton.mercado@gmail.com>
2025-07-09 17:29:04 -0400

    refactored stubby and resolved

--- NixOS/networking.nix
+++ NixOS/networking.nix
@@ -14,5 +14,39 @@ in
     nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
   };
 
-    
+  services ={
+    resolved = {
+      enable = true;
+      dnssec = "true";
+      domains = [ "~." ];
+      fallbackDns = [
+        "9.9.9.9#dns.quad9.net" # Quad9 as a fallback
+        "8.8.8.8#dns.google"   # Google as a fallback
+      ];
+    };    
+    # Stubby DNS over TLS
+      # It uses Cloudflare's DNS servers with DNSSEC validation.
+      # You can customize the upstream servers and settings as needed.
+      # For more information, see:  https://nixos.wiki/wiki/Encrypted_DNS
+    stubby = {
+      enable = true;
+      settings = pkgs.stubby.passthru.settingsExample // {
+        upstream_recursive_servers = [{
+          address_data = "1.1.1.1";
+          tls_auth_name = "cloudflare-dns.com";
+          tls_pubkey_pinset = [{
+            digest = "sha256";
+            value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
+          }];
+        } {
+          address_data = "1.0.0.1";
+          tls_auth_name = "cloudflare-dns.com";
+          tls_pubkey_pinset = [{
+            digest = "sha256";
+            value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
+          }];
+        }];
+      };
+    };
+  };
 }
